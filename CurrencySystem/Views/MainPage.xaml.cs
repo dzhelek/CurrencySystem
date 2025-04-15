2ed81074c8aa5722ba92c95ee753b07f1c03054d
Yoan Dzhelekarski <yoan.dzhelekarski@gmail.com>
2025-04-16 00:46:53 +0300

    create the view template

--- CurrencySystem/Views/MainPage.xaml.cs
+++ CurrencySystem/Views/MainPage.xaml.cs
@@ -2,23 +2,25 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void OnConvertButtonClicked(object sender, EventArgs e)
         {
-            count++;
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+        }
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        private void OnSubmitFeedbackButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnHelpButtonClicked(object sender, EventArgs e)
+        {
+
         }
     }
 
